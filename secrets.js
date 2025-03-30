54d192e21387dfdc9963e2a35539d06bbbe9d96c
didi8bg <didi8bg@gmail.com>
2025-03-30 12:50:21 +0300

    New Versio, New Logic

---
+++ secrets.js
@@ -0,0 +1,9 @@
+// secrets.js
+
+module.exports = {
+  GATE_API_KEY: '706119df4f51a5e2dbbd77b717f5c528',
+  GATE_API_SECRET: '62ab4ac98000edcb80e9852e74e727b168595d780adea34ebffb56c419c5dc7f',
+
+  // ✅ Add your actual MongoDB URI below
+  MONGO_URI: 'mongodb://localhost:27017' // or your remote URI if using MongoDB Atlas
+};
